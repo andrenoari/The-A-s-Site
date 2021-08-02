5d23ae11b0edd22612f22e5724e147845b22a370
Ariful Islam Khan <61895871+andrenoari@users.noreply.github.com>
2021-08-02 17:59:58 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function Home() {
         <p className="description">
          I don't know who am I but sure I'm giving my test for the hereafter 😀 
        </p>
-    <p>I'm available on Social Media, You can stalk me there 😋</p>
-<p>✨ <a title="Instagram" href="https://www.instagram.com/andrenoari/">Instagram</a> 🕊️ <a title="Twitter" href="https://www.twitter.com/andrenoari/">Twitter</a></p>
+    <h6>I'm available on Social Media, You can stalk me there 😋</h6>
+<p>✨ <a title="Instagram" href="https://www.instagram.com/andrenoari/">Instagram</a>&nbsp; &nbsp;🕊️ <a title="Twitter" href="https://www.twitter.com/andrenoari/">Twitter</a></p>
       </main>
     
       <Footer />
